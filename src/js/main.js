3deb5ded1602a73b35eef9420befd83e364ea348
Shreya Shah <shreya@darwindigital.com>
2024-10-17 11:07:38 +0530

    Vertical scrollbar fixes, cta position based on product popup visibility

--- src/js/main.js
+++ src/js/main.js
@@ -89,8 +89,9 @@ $(document).ready(function () {
     $(".story__prev").removeClass("active-popup");
     $(".story__next").removeClass("active-popup");
 
-    $('.story__product-popup-cta').addClass("active");
-    $('.story__product-popup').addClass('active');
+    $(".story__product-popup-cta").addClass("active");
+    $(".story__product-popup").addClass("active");
+
     closeStoryPopup();
   });
 
@@ -298,8 +299,15 @@ $(document).ready(function () {
   });
 
   // Story slider - Product popup
-  $('.story__product-popup-cta').on('click', function() {
-    $(this).toggleClass('active');
-    $(this).next('.story__product-popup').toggleClass('active');
+  $(".story__product-popup-cta").on("click", function () {
+    $(this).toggleClass("active");
+    $(this).next(".story__product-popup").toggleClass("active");
+
+    // Add active class to cta elements when the product popup is active
+    if ($(this).next(".story__product-popup").hasClass("active")) {
+      $(".story__modal .modal-cta .cta").addClass("active");
+    } else {
+      $(".story__modal .modal-cta .cta").removeClass("active");
+    }
   });
 });
